4c08f418c0a30023c059583a6d547e242d245829
sgx <sunguangxu@aliyun.com>
2019-03-01 17:25:22 +0800

    增加了RabbitAMQP的主要类的说明

--- src/main/java/com/gx/ms/SpringbootMessageApplication.java
+++ src/main/java/com/gx/ms/SpringbootMessageApplication.java
@@ -5,14 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 自动配置
- * {@link org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration}
- * {@link org.springframework.amqp.rabbit.core.RabbitTemplate}
- * {@link com.rabbitmq.client.ConnectionFactory}
- * {@link org.springframework.amqp.core.AmqpAdmin}
- * {@link EnableRabbit}
+ * <p>
+ * {@link org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration} ： 引入ampq依赖以后，会自动加载RabbitAmqp的配置
+ * <p>
+ * {@link org.springframework.amqp.rabbit.core.RabbitTemplate} ：提供了操作RabbitAMQP的模板和JdbcTemplate模板类似
+ * <p>
+ * {@link com.rabbitmq.client.ConnectionFactory} ：提供了连接RabbitAMQP的服务的连接工厂
+ * <p>
+ * {@link org.springframework.amqp.core.AmqpAdmin} ：提供了监听RabbitAMQP的消息实现
+ * <p>
+ * {@link EnableRabbit} ：如果要开启订阅消息，则必须使用这个
  */
-@EnableRabbit// 开启注解的RabbitMQ模式
+@EnableRabbit// 开启注解的RabbitMQ模式，主要是为了监听队列中的消息，以便取出消息
 @SpringBootApplication
 public class SpringbootMessageApplication {
 
